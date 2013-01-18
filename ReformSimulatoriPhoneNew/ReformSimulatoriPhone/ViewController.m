719f516ec23ca169022bd78ba01960fb941c3e92
GhostIntheSpace <minimalidol@gmail.com>
2013-01-18 14:33:13 +0900

    git test 當間

--- ReformSimulatoriPhoneNew/ReformSimulatoriPhone/ViewController.m
+++ ReformSimulatoriPhoneNew/ReformSimulatoriPhone/ViewController.m
@@ -25,6 +25,7 @@ int indexCount;
 - (id)init {
 	self = [super init];
 	if (self) {
+        NSLog(@"tomagittest");//gitの使用確認の為(一時的な確認のため、後削除します)
 		titleArray = [NSMutableArray arrayWithObjects:@"1.JPG", @"2.JPG", @"3.JPG", @"4.JPG", @"5.JPG", @"6.JPG", @"7.JPG", @"8.JPG", @"9.JPG", @"10.JPG", @"11.JPG", @"12.JPG", @"13.JPG", @"14.JPG", @"15.JPG", nil];
 		indexCount = 0;
 	}
