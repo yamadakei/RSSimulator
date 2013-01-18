3a235bbf71b77a98c1d3717f1d21b4f4ef84b065
yamadakei <yamadakei1979@gmail.com>
2013-01-18 15:47:42 +0900

    Delegateが走らない問題解決

--- ReformSimulatoriPhoneNew/ReformSimulatoriPhone/ViewController.m
+++ ReformSimulatoriPhoneNew/ReformSimulatoriPhone/ViewController.m
@@ -39,6 +39,9 @@ int indexCount;
     [super viewDidLoad];
     AppDelegate* delegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
     
+    titleArray = [NSMutableArray arrayWithObjects:@"1.JPG", @"2.JPG", @"3.JPG", @"4.JPG", @"5.JPG", @"6.JPG", @"7.JPG", @"8.JPG", @"9.JPG", @"10.JPG", @"11.JPG", @"12.JPG", @"13.JPG", @"14.JPG", @"15.JPG", nil];
+    indexCount = 0;
+    
     // ビデオキャプチャデバイスの取得
     AVCaptureDevice* device;
     device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
